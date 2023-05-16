311053ae34e24a7a70ef459328da8a1ebcc915d2
AmeyPank <pankantiameya@gmail.com>
2023-05-16 12:30:47 +0530

    This is the content for Tab 1.

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,9 @@ import Tabs from './Tabs';
 
 const App = () => {
   const tabs = [
-    { title: 'Tab 1', content: 'This is the content of Tab 1.' },
-    { title: 'Tab 2', content: 'This is the content of Tab 2.' },
-    { title: 'Tab 3', content: 'This is the content of Tab 3.' },
+    { title: 'Tab 1', content: 'This is the content for Tab 1.' },
+    { title: 'Tab 2', content: 'This is the content for Tab 1.' },
+    { title: 'Tab 3', content: 'This is the content for Tab 1.' },
   ];
 
   return (
